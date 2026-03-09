f711ddbba11c4d570cfd53f8fd5ef5d4edd9dc18
Usamot <57453355+Usamot@users.noreply.github.com>
2026-03-09 09:23:35 +0100

    Add whitenoise for static files

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -138,4 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
